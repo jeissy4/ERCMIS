2a335e06b3dbca3a2b228e77fa10e8fb528ad8d0
wanjir0 <charlyshiro4@gmail.com>
2025-07-30 12:26:08 +0300

    social services

--- ERCMIS-FRONTEND/src/App.jsx
+++ ERCMIS-FRONTEND/src/App.jsx
@@ -1,11 +1,19 @@
-
-
+//import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AmbulanceServiceFeePage from "./pages/SocialServices/AmbulanceServiceFeePage";
+import HospitalFeePage from "./pages/SocialServices/HospitalFeePage";
+import MaternityFeePage from "./pages/SocialServices/MaternityAndOutpatientFeePage";
 function App() {
 
   return (
-    <>
-    
-    </>
+  
+    <BrowserRouter>
+ <Routes>
+      <Route path="/ambulance-service-fee" element={<AmbulanceServiceFeePage />} />
+      <Route path="/hospital-fee" element={<HospitalFeePage />} />
+      <Route path="/maternity-fee" element={<MaternityFeePage />} />
+ </Routes>
+    </BrowserRouter>
   )
 }
 
