19fb0083a2562b26745d0bb81b3495074f569a0c
hoseamaganga1 <151690410+hoseamaganga1@users.noreply.github.com>
2025-07-31 15:55:18 +0300

    Add files via upload

--- ERCMIS-FRONTEND/src/App.jsx
+++ ERCMIS-FRONTEND/src/App.jsx
@@ -1,29 +1,32 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import SlaughterhousePage from './pages/AgricultureLivestock/SlaughterhousePage';
-import CropInspectionCertPage from './pages/AgricultureLivestock/CropInspectionCertPage';
-import LivestockMovementPage from './pages/AgricultureLivestock/LivestockMovementPage';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import SlaughterhousePage from "./pages/AgricultureLivestock/SlaughterhousePage";
+import CropInspectionCertPage from "./pages/AgricultureLivestock/CropInspectionCertPage";
+import LivestockMovementPage from "./pages/AgricultureLivestock/LivestockMovementPage";
 //import AgricultureLivestockNavbar from './components/AgricultureLivestockComponents/AgricultureLivestockNavbar';
-import Navbar from './components/AgricultureLivestockComponents/AgricultureLivestockNavbar';
-import Header from './components/Header';
-import Footer from './components/Footer'; 
-function App() {
+import Navbar from "./components/AgricultureLivestockComponents/AgricultureLivestockNavbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+//
+import LoadingZonePermitPage from "./pages/TransportInfrastructure/LoadingZonePermitPage";
+import ParkingFeePage from "./pages/TransportInfrastructure/ParkingFeePage";
+import PSVLicenseFeePage from "./pages/TransportInfrastructure/PSVLicenseFeePage";
 
+function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <Navbar />
-
-     <Routes>
-      <Route path="/slaughterhouse" element={<SlaughterhousePage />} />
-      <Route path="/crop-inspection" element={<CropInspectionCertPage />} />
-      <Route path="/livestock-movement" element={<LivestockMovementPage />} />
-     </Routes>
-    <Footer />
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/slaughterhouse" element={<SlaughterhousePage />} />
+        <Route path="/crop-inspection" element={<CropInspectionCertPage />} />
+        <Route path="/livestock-movement" element={<LivestockMovementPage />} />
+        <Route path="/loadingzonepermit" element={<LoadingZonePermitPage />} />
+        <Route path="/parkingFee" element={<ParkingFeePage />} />
+        <Route path="/psvicensefee" element={<PSVLicenseFeePage />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-
-    
-    
-  )
+  );
 }
 
-export default App
+export default App;
