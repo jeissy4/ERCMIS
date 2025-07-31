3347c87ff4c24f4dc4fe404ea4a2a20058b17f74
AntonyRoudo <131287862+AntonyRoudo@users.noreply.github.com>
2025-07-31 15:49:43 +0300

    Add files via upload

--- ERCMIS-FRONTEND/src/App.jsx
+++ ERCMIS-FRONTEND/src/App.jsx
@@ -1,11 +1,28 @@
-
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import SlaughterhousePage from './pages/AgricultureLivestock/SlaughterhousePage';
+import CropInspectionCertPage from './pages/AgricultureLivestock/CropInspectionCertPage';
+import LivestockMovementPage from './pages/AgricultureLivestock/LivestockMovementPage';
+//import AgricultureLivestockNavbar from './components/AgricultureLivestockComponents/AgricultureLivestockNavbar';
+import Navbar from './components/AgricultureLivestockComponents/AgricultureLivestockNavbar';
+import Header from './components/Header';
+import Footer from './components/Footer'; 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+    <Header />
+    <Navbar />
+
+     <Routes>
+      <Route path="/slaughterhouse" element={<SlaughterhousePage />} />
+      <Route path="/crop-inspection" element={<CropInspectionCertPage />} />
+      <Route path="/livestock-movement" element={<LivestockMovementPage />} />
+     </Routes>
+    <Footer />
+    </BrowserRouter>
+
     
-    </>
+    
   )
 }
 
